7c622930720818de175915183c5a8587b5defe95
Marcin Jedrzejewski <marcinje@gmail.com>
2023-01-20 14:07:35 +0100

    Day 11

--- bin/adventofcode_2022.dart
+++ bin/adventofcode_2022.dart
@@ -10,6 +10,7 @@ import 'package:adventofcode_2022/day07.dart';
 import 'package:adventofcode_2022/day08.dart';
 import 'package:adventofcode_2022/day09.dart';
 import 'package:adventofcode_2022/day10.dart';
+import 'package:adventofcode_2022/day11.dart';
 
 import 'dart:developer';
 import 'package:worker_manager/worker_manager.dart';
@@ -28,6 +29,7 @@ void main(List<String> arguments) async {
     Day08(),
     Day09(),
     Day10(),
+    Day11(),
   ];
 
   for (var day in days) {
